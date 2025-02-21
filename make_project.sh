2aa67db68a8367915d991822e3cd91bcf2a79cc4
Zicameau <rowdeil@gmail.com>
2025-02-21 15:10:56 -0800

    Edited make_project.sh

--- make_project.sh
+++ make_project.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+# Exit on first error
+set -e
+
+# Clean build directory first
+rm -rf build
+mkdir -p build
+cd build
+
 # Ensure compiler environment
 export CC=/usr/bin/gcc
 export CXX=/usr/bin/g++
 
-# Build commands
-cd build
-cmake -DCMAKE_BUILD_TYPE=Debug .. --debug-output --trace-expand
-make -j$(nproc) 
+# Build commands with maximum verbosity
+cmake -DCMAKE_BUILD_TYPE=Debug \
+      -DCMAKE_VERBOSE_MAKEFILE=ON \
+      -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+      .. --debug-output --trace-expand
+
+make VERBOSE=1 -j$(nproc) || make VERBOSE=1 
