922d6dca8c6092a2829dc19984789c532bb8fa29
Zicameau <rowdeil@gmail.com>
2025-02-20 20:27:20 -0800

    Replaces cp --silent with cp -f which is more widely supported

--- cmake/Modules/AXShaderHelpers.cmake
+++ cmake/Modules/AXShaderHelpers.cmake
@@ -6,10 +6,11 @@ function(ax_target_compile_shaders target)
     # Call the original function but redirect output to /dev/null to achieve silence
     _ax_target_compile_shaders(${target} ${ARGN})
     
-    # Replace cp --silent with cp 2>/dev/null
     if(UNIX)
-        execute_process(
-            COMMAND sed -i "s/cp --silent/cp 2>\\/dev\\/null/g" 
+        # Add a custom command to modify the build.make file after CMake generation
+        add_custom_command(
+            TARGET ${target} PRE_BUILD
+            COMMAND sed -i "s/cp --silent/cp -f/g" 
             "${CMAKE_BINARY_DIR}/engine/axmol/core/CMakeFiles/axmol.dir/build.make"
         )
     endif()
