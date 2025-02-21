4a973c29a26bff513153038fe1b347b9cb034ae6
Zicameau <rowdeil@gmail.com>
2025-02-20 20:32:52 -0800

    We're still using sed to modify the build file, but we're directly replacing cp --silent with cp -f

--- cmake/Modules/AXShaderHelpers.cmake
+++ cmake/Modules/AXShaderHelpers.cmake
@@ -3,16 +3,15 @@ include("${CMAKE_SOURCE_DIR}/external/axmol/cmake/Modules/AXSLCC.cmake")
 
 # Override the shader compilation function with our wrapper
 function(ax_target_compile_shaders target)
-    # Create the output directory first
+    # Create the output directory
     file(MAKE_DIRECTORY "${CMAKE_BINARY_DIR}/runtime/axslc")
     
     # Call the original function
     _ax_target_compile_shaders(${target})
     
     if(UNIX AND NOT APPLE)
-        # Fix the cp commands in the build file
-        add_custom_command(
-            TARGET ${target} PRE_BUILD
+        # Modify the build commands to use cp -f instead of cp --silent
+        add_custom_command(TARGET ${target} PRE_BUILD
             COMMAND sed -i "s/cp --silent/cp -f/g" 
             "${CMAKE_BINARY_DIR}/engine/axmol/core/CMakeFiles/axmol.dir/build.make"
         )
